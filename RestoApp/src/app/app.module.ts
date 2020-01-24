25f6bf5d0df9d23fef13c2f09365d19310e7eace
flamme <aflamme@actiris.be>
2020-01-24 14:07:17 +0100

    ✨Routing , navigation , fisrt MatCard

--- RestoApp/src/app/app.module.ts
+++ RestoApp/src/app/app.module.ts
@@ -4,17 +4,43 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'resevation', component: ReservationComponent },
+    { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, MainNavComponent, HomeComponent, MenuComponent, ReservationComponent, ContactComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        RouterModule.forRoot(routes),
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
